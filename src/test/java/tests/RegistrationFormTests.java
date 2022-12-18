5ad1dda6b83fbe41e086ed631510358645fed36d
Yuliya.Belyaeva <ybelyaeva8632@gmail.com>
2022-12-18 12:43:02 +0400

    Generate test data via Faker

--- src/test/java/tests/RegistrationFormTests.java
+++ src/test/java/tests/RegistrationFormTests.java
@@ -3,49 +3,35 @@ package tests;
 import org.junit.jupiter.api.Test;
 
 public class RegistrationFormTests extends TestBase {
+
+
     @Test
     void fillFormTest() {
-        String firstName = "Maria",
-                lastName = "Ivanova",
-                userEmail = "test@gmail.com",
-                gender = "Female",
-                userNumber = "7999999999",
-                dayOfBirth = "14",
-                monthOfBirth = "April",
-                yearOfBirth = "1997",
-                subject = "Maths",
-                hobby = "Sports",
-                pictureDirectory = "src/test/resources/picture.png",
-                address = "75 Park Place, 15",
-                state = "NCR",
-                city = "Delhi";
-
         registrationPage.openPage()
-                .setFirstName(firstName)
-                .setLastName(lastName)
-                .setEmail(userEmail)
-                .setGender(gender)
-                .setNumber(userNumber)
-                .setBirthDate(dayOfBirth, monthOfBirth, yearOfBirth)
-                .setSubject(subject)
-                .setHobby(hobby)
-                .uploadPicture(pictureDirectory)
-                .setAddress(address)
-                .setState(state)
-                .setCity(city)
+                .setFirstName(testData.firstName)
+                .setLastName(testData.lastName)
+                .setEmail(testData.userEmail)
+                .setGender(testData.gender)
+                .setNumber(testData.userNumber)
+                .setBirthDate(testData.dayOfBirth, testData.monthOfBirth, testData.yearOfBirth)
+                .setSubject(testData.subject)
+                .setHobby(testData.hobby)
+                .uploadPicture(testData.pictureDirectory)
+                .setAddress(testData.address)
+                .setState(testData.state)
+                .setCity(testData.city)
                 .clickSubmitButton()
                 .verifyModalResultsAppears()
-                .verifyFormResult("Student Name", firstName + " " + lastName)
-                .verifyFormResult("Student Email", userEmail)
-                .verifyFormResult("Gender", gender)
-                .verifyFormResult("Mobile", userNumber)
-                .verifyFormResult("Mobile", userNumber)
-                .verifyFormResult("Date of Birth", dayOfBirth + " " + monthOfBirth + "," + yearOfBirth)
-                .verifyFormResult("Subjects", subject)
-                .verifyFormResult("Hobbies", hobby)
-                .verifyFormResult("Picture", pictureDirectory.replaceAll("src/test/resources/",""))
-                .verifyFormResult("Address", address)
-                .verifyFormResult("State and City", state + " " + city)
+                .verifyFormResult("Student Name", testData.firstName + " " + testData.lastName)
+                .verifyFormResult("Student Email", testData.userEmail)
+                .verifyFormResult("Gender", testData.gender)
+                .verifyFormResult("Mobile", testData.userNumber)
+                .verifyFormResult("Date of Birth", testData.dayOfBirth + " " + testData.monthOfBirth + "," + testData.yearOfBirth)
+                .verifyFormResult("Subjects", testData.subject)
+                .verifyFormResult("Hobbies", testData.hobby)
+                .verifyFormResult("Picture", testData.pictureDirectory.replaceAll("src/test/resources/",""))
+                .verifyFormResult("Address", testData.address)
+                .verifyFormResult("State and City", testData.state + " " + testData.city)
                 .clickCloseButton();
     }
 }
---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,39 @@
+package tests;
+
+import com.github.javafaker.Faker;
+
+import java.text.SimpleDateFormat;
+import java.util.Random;
+
+public class TestData {
+    Faker faker = new Faker();
+    static Random random = new Random();
+    static String[] genders = {"Male", "Female", "Other"},
+                    months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"},
+                    subjects = {"Accounting", "Arts", "Biology", "Chemistry", "English", "History"},
+                    hobbies = {"Sports", "Reading", "Music"},
+                    citiesNCR = {"Delhi", "Gurgaon", "Noida"};
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
+    String[] birthDay = dateFormat.format(faker.date().birthday()).split(" ");
+
+    String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            userEmail = faker.internet().emailAddress(),
+            gender = randomItem(genders),
+            userNumber = faker.phoneNumber().subscriberNumber(10),
+            dayOfBirth = birthDay[0],
+            monthOfBirth = birthDay[1],
+            yearOfBirth = birthDay[2],
+            subject = randomItem(subjects),
+            hobby = randomItem(hobbies),
+            pictureDirectory = "src/test/resources/picture.png",
+            address = faker.address().fullAddress(),
+            state = "NCR",
+            city = randomItem(citiesNCR);
+
+    public static String randomItem(String[] list) {
+        int i = random.nextInt(list.length);
+        return list[i];
+    }
+}
